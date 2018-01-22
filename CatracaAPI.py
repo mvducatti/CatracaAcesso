2a5f6ffc0ddc40a0b988a917feccee9db440a92e
Marcos Ducatti <17991500+mvducatti@users.noreply.github.com>
2018-01-22 16:43:10 -0200

    Update CatracaAPI.py

--- CatracaAPI.py
+++ CatracaAPI.py
@@ -7,11 +7,17 @@ class CatracaAPI:
         self.COD_PESSOA = COD_PESSOA
 
     def getJsonCatraca(self):
-        url = "http://c3-srv-bio01:5002/catraca/%s/%s" %(self.COD_PESSOA,self.COD_DEPARTAMENTO)
-        response = urllib.urlopen(url)
-        data = json.loads(response.read())
+        try:
+            url = "http://c3-srv-bio01:5002/catraca/%s/%s" %(self.COD_PESSOA,self.COD_DEPARTAMENTO)
+            response = urllib.urlopen(url)
+            data = json.loads(response.read())
+        except Exception:
+          print "Erro de Conexao com o Servidor"
         return data
 
-    def catracaShow(self):        
-        jsonCatraca = self.getJsonCatraca()['catraca'][0]        
-        print "%s  %s  %s  %s  %s  %s  %s  %s" % (jsonCatraca['COD_LIBERACAO'],jsonCatraca['COD_EVENTO'],jsonCatraca['VALOR_EVENTO'], jsonCatraca['COD_VIA_CARTEIRA'], jsonCatraca['MSG1'],jsonCatraca['MSG2'] ,jsonCatraca['NOM_PESSOA'], jsonCatraca['DES_EVENTO'])
+    def catracaShow(self):   
+        try:
+            jsonCatraca = self.getJsonCatraca()['catraca'][0] 
+            print "%s  %s  %s  %s  %s  %s  %s  %s" % (jsonCatraca['COD_LIBERACAO'],jsonCatraca['COD_EVENTO'],jsonCatraca['VALOR_EVENTO'], jsonCatraca['COD_VIA_CARTEIRA'], jsonCatraca['MSG1'],jsonCatraca['MSG2'] ,jsonCatraca['NOM_PESSOA'], jsonCatraca['DES_EVENTO'])
+        except Exception:
+            print "Problema em exibir o resultado"
